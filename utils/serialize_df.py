f69fe702b01f5850ecf53e171c8d6e31ceb8e186
h-pgy <hpougy@gmail.com>
2025-04-02 23:16:17 -0300

    eda simples para selecao de variaveis

--- utils/serialize_df.py
+++ utils/serialize_df.py
@@ -1,8 +1,9 @@
 from config import DATA_PATH
 import os
+import pandas as pd
 
 
-def solve_fpath(fname:str, folder=DATA_PATH)->str:
+def solve_fpath(fname:str, folder:str=DATA_PATH)->str:
 
     if not os.path.exists(folder):
         os.mkdir(folder)
@@ -20,3 +21,13 @@ def df_to_parquet(df, fname: str, folder=DATA_PATH) -> str:
     df.to_parquet(fpath, index=False)
 
     return fpath
+
+
+def read_parquet_df(fname:str, folder:str=DATA_PATH)->pd.DataFrame:
+
+    if not fname.endswith('.parquet'):
+        raise ValueError(f'File must be .parquet')
+    
+    fpath: str = solve_fpath(fname, folder)
+
+    return pd.read_parquet(fpath)
